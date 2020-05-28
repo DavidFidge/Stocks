55c13fef32937f6aa7ecef1c2ed9c35169e9e2b7
David Fidge <dfidge@internode.on.net>
2020-05-28 21:42:36 +0930

    stock page

--- src/Stock/Stock.svelte
+++ src/Stock/Stock.svelte
@@ -5,4 +5,14 @@
 
 </style>
 
-<div>Stock</div>
+<div>
+    
+    
+    <h3>Search for a stock: </h3>
+
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" style="width: 500px" type="search" placeholder="Asx code or company name" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
+</div>
