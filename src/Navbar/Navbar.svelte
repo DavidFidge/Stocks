a26c013f7efce7ceb2a91a144457399e9d64ef42
David Fidge <dfidge@internode.on.net>
2020-06-24 21:15:03 +0930

    Changing location changes highlighted navbar item

--- src/Navbar/Navbar.svelte
+++ src/Navbar/Navbar.svelte
@@ -1,30 +1,47 @@
 <script>
     import { links } from 'svelte-routing';
 
-    /*$: {
+    import { onMount } from 'svelte';
 
-        console.log(url);
+    let updateActiveNavbarItem = function()
+    {
+        let pathname = window.location.pathname;
+
+        if (pathname === '/')
+            pathname = '/home';
+
         let navbarItems = window.$("#navbarSupportedContent li a");
 
-         for (let navbarItem of navbarItems)
-         {
-            let pattern = new RegExp(navbarItem.html().toLower());
+        for (let navbarItem of navbarItems)
+        {
+            let pattern = new RegExp(window.$(navbarItem).text().toLowerCase());
 
-            let match = url.test(pattern);
+            let match = pattern.test(pathname);
 
-            if (match && navbarItem.hasClass("active"))
+            if (match && !window.$(navbarItem).hasClass("active"))
             {
-                navbarItems.removeClass("active");
-                navbarItem.addClass("active");
+                window.$(navbarItems).removeClass("active");
+                window.$(navbarItem).addClass("active");
+                window.$(navbarItems).blur();
+
                 break;
             }
-         }
-    }*/
+        }
+    
+        setTimeout(updateActiveNavbarItem, 100);
+    };
+
+    onMount(() => 
+    {
+        // The only way to detect changes to window.location seems to be to poll.
+        // An alternative way would be to plumb it in the routing system or a link component
+        // as well as handling browser back and forward.  At the moment polling is a lot simpler.
+        // https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes/33668370
+        setTimeout(updateActiveNavbarItem, 100);
+    });
+
 </script>
 
-<style>
-</style>
-<p>{url}</p>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" use:links>
     <a class="navbar-brand" href="/"><img src="./img/stock.png" class="mr-1" alt="Stocks"/>Portfolio Manager</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,8 +50,8 @@
     
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home {url}</a>
+        <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="/portfolio">Portfolio</a>
